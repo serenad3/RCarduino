83bf405d77bb2ca5695f7cd9ec9899b9db36e2f7
serenad3 <serenade_3@yahoo.com>
2015-03-16 13:29:21 +0100

    Update rcGW.ino

--- rcGW.ino
+++ rcGW.ino
@@ -1,31 +1,27 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <stdlib.h>
-#include "nRF24L01.h"
-#include "RF24.h"
-
-RF24 radio(8,9);
-const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
-
-byte mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDA, 0x02};
-IPAddress ip(192,168,1,177);
-EthernetServer server(80);
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <RF24_config.h>
 
 char c;
 char command[16];
 boolean incoming = 0;
 
+RF24 radio(8,9);
+const uint64_t pipes = 0xE8E8F0F0E1LL;
+
+byte mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDA, 0x02};
+IPAddress ip(192,168,1,177);
+EthernetServer server(80);
+
 void setup(){
-  
+  Serial.begin(9600);
   Ethernet.begin(mac, ip);
   server.begin();
   radio.begin();
-  radio.openWritingPipe(pipes[0]);
-  radio.openReadingPipe(1,pipes[1]);
-  radio.startListening();
-// For debug
-  Serial.begin(9600);
-
+  radio.openWritingPipe(pipes);
 }
 
 void loop(){
@@ -39,17 +35,12 @@ void loop(){
         if (c == '$') incoming = 1;
         if (incoming == 1){
          
-// Get the commands:
           for (int x=0 ; x<16 ; x++){
           c = client.read();
           command[x] = c;
-          radio.write( &c, sizeof(unsigned long) );
-// For debug
-          Serial.print(c);
-              
+          radio.write(command, 16);
 
-
-// Send DATA to nrf24l01+ destination
+          Serial.print(command);
 
       }
       incoming = 0;
@@ -60,5 +51,8 @@ void loop(){
  
 delay(1);
 client.stop();
+radio.powerDown(); 
+delay(1000);
+radio.powerUp();
 
 } 
